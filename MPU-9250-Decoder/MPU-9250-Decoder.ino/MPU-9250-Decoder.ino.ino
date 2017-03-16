9745fd6cf8046f6bdbcc4e44b568e20a17660aaa
Harry Beadle <harry.beadle@gmail.com>
2017-03-16 20:09:14 +0000

    Final Version used in D4

--- MPU-9250-Decoder/MPU-9250-Decoder.ino/MPU-9250-Decoder.ino.ino
+++ MPU-9250-Decoder/MPU-9250-Decoder.ino/MPU-9250-Decoder.ino.ino
@@ -53,7 +53,7 @@ void setup()
 {
   Wire.begin();
   // TWBR = 12;  // 400 kbit/sec I2C speed
-  Serial.begin(38400);
+  Serial.begin(57600);
 
   // Set up the interrupt pin, its set as active high, push-pull
   pinMode(intPin, INPUT);
@@ -261,122 +261,6 @@ void loop()
                          myIMU.gy*DEG_TO_RAD, myIMU.gz*DEG_TO_RAD, myIMU.my,
                          myIMU.mx, myIMU.mz, myIMU.deltat);
 
-  if (!AHRS)
-  {
-    myIMU.delt_t = millis() - myIMU.count;
-    if (myIMU.delt_t > 500)
-    {
-      if(SerialDebug)
-      {
-        // Print acceleration values in milligs!
-        Serial.print("X-acceleration: "); Serial.print(1000*myIMU.ax);
-        Serial.print(" mg ");
-        Serial.print("Y-acceleration: "); Serial.print(1000*myIMU.ay);
-        Serial.print(" mg ");
-        Serial.print("Z-acceleration: "); Serial.print(1000*myIMU.az);
-        Serial.println(" mg ");
-
-        // Print gyro values in degree/sec
-        Serial.print("X-gyro rate: "); Serial.print(myIMU.gx, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Y-gyro rate: "); Serial.print(myIMU.gy, 3);
-        Serial.print(" degrees/sec ");
-        Serial.print("Z-gyro rate: "); Serial.print(myIMU.gz, 3);
-        Serial.println(" degrees/sec");
-
-        // Print mag values in degree/sec
-        Serial.print("X-mag field: "); Serial.print(myIMU.mx);
-        Serial.print(" mG ");
-        Serial.print("Y-mag field: "); Serial.print(myIMU.my);
-        Serial.print(" mG ");
-        Serial.print("Z-mag field: "); Serial.print(myIMU.mz);
-        Serial.println(" mG");
-
-        myIMU.tempCount = myIMU.readTempData();  // Read the adc values
-        // Temperature in degrees Centigrade
-        myIMU.temperature = ((float) myIMU.tempCount) / 333.87 + 21.0;
-        // Print temperature in degrees Centigrade
-        Serial.print("Temperature is ");  Serial.print(myIMU.temperature, 1);
-        Serial.println(" degrees C");
-      }
-
-#ifdef LCD
-      display.clearDisplay();
-      display.setCursor(0, 0); display.print("MPU9250/AK8963");
-      display.setCursor(0, 8); display.print(" x   y   z  ");
-
-      display.setCursor(0,  16); display.print((int)(1000*myIMU.ax));
-      display.setCursor(24, 16); display.print((int)(1000*myIMU.ay));
-      display.setCursor(48, 16); display.print((int)(1000*myIMU.az));
-      display.setCursor(72, 16); display.print("mg");
-
-      display.setCursor(0,  24); display.print((int)(myIMU.gx));
-      display.setCursor(24, 24); display.print((int)(myIMU.gy));
-      display.setCursor(48, 24); display.print((int)(myIMU.gz));
-      display.setCursor(66, 24); display.print("o/s");
-
-      display.setCursor(0,  32); display.print((int)(myIMU.mx));
-      display.setCursor(24, 32); display.print((int)(myIMU.my));
-      display.setCursor(48, 32); display.print((int)(myIMU.mz));
-      display.setCursor(72, 32); display.print("mG");
-
-      display.setCursor(0,  40); display.print("Gyro T ");
-      display.setCursor(50,  40); display.print(myIMU.temperature, 1);
-      display.print(" C");
-      display.display();
-#endif // LCD
-
-      myIMU.count = millis();
-      digitalWrite(myLed, !digitalRead(myLed));  // toggle led
-    } // if (myIMU.delt_t > 500)
-  } // if (!AHRS)
-  else
-  {
-    // Serial print and/or display at 0.5 s rate independent of data rates
-    myIMU.delt_t = millis() - myIMU.count;
-
-    // update LCD once per half-second independent of read rate
-    if (1)
-    {
-      if(!SerialDebug)
-      {
-        Serial.print("ax = "); Serial.print((int)1000*myIMU.ax);
-        Serial.print(" ay = "); Serial.print((int)1000*myIMU.ay);
-        Serial.print(" az = "); Serial.print((int)1000*myIMU.az);
-        Serial.println(" mg");
-
-        Serial.print("gx = "); Serial.print( myIMU.gx, 2);
-        Serial.print(" gy = "); Serial.print( myIMU.gy, 2);
-        Serial.print(" gz = "); Serial.print( myIMU.gz, 2);
-        Serial.println(" deg/s");
-
-        Serial.print("mx = "); Serial.print( (int)myIMU.mx );
-        Serial.print(" my = "); Serial.print( (int)myIMU.my );
-        Serial.print(" mz = "); Serial.print( (int)myIMU.mz );
-        Serial.println(" mG");
-
-        Serial.print("q0 = "); Serial.print(*getQ());
-        Serial.print(" qx = "); Serial.print(*(getQ() + 1));
-        Serial.print(" qy = "); Serial.print(*(getQ() + 2));
-        Serial.print(" qz = "); Serial.println(*(getQ() + 3));
-      }
-
-// Define output variables from updated quaternion---these are Tait-Bryan
-// angles, commonly used in aircraft orientation. In this coordinate system,
-// the positive z-axis is down toward Earth. Yaw is the angle between Sensor
-// x-axis and Earth magnetic North (or true North if corrected for local
-// declination, looking down on the sensor positive yaw is counterclockwise.
-// Pitch is angle between sensor x-axis and Earth ground plane, toward the
-// Earth is positive, up toward the sky is negative. Roll is angle between
-// sensor y-axis and Earth ground plane, y-axis up is positive roll. These
-// arise from the definition of the homogeneous rotation matrix constructed
-// from quaternions. Tait-Bryan angles as well as Euler angles are
-// non-commutative; that is, the get the correct orientation the rotations
-// must be applied in the correct order which for this configuration is yaw,
-// pitch, and then roll.
-// For more see
-// http://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles
-// which has additional links.
       myIMU.yaw   = atan2(2.0f * (*(getQ()+1) * *(getQ()+2) + *getQ() *
                     *(getQ()+3)), *getQ() * *getQ() + *(getQ()+1) * *(getQ()+1)
                     - *(getQ()+2) * *(getQ()+2) - *(getQ()+3) * *(getQ()+3));
@@ -414,57 +298,8 @@ void loop()
       Serial.write(YAW_S);
       Serial.write((char) yaw_send >> 8);
       Serial.write((char) yaw_send);
-        
-
-#ifdef LCD
-      display.clearDisplay();
-
-      display.setCursor(0, 0); display.print(" x   y   z  ");
-
-      display.setCursor(0,  8); display.print((int)(1000*myIMU.ax));
-      display.setCursor(24, 8); display.print((int)(1000*myIMU.ay));
-      display.setCursor(48, 8); display.print((int)(1000*myIMU.az));
-      display.setCursor(72, 8); display.print("mg");
-
-      display.setCursor(0,  16); display.print((int)(myIMU.gx));
-      display.setCursor(24, 16); display.print((int)(myIMU.gy));
-      display.setCursor(48, 16); display.print((int)(myIMU.gz));
-      display.setCursor(66, 16); display.print("o/s");
-
-      display.setCursor(0,  24); display.print((int)(myIMU.mx));
-      display.setCursor(24, 24); display.print((int)(myIMU.my));
-      display.setCursor(48, 24); display.print((int)(myIMU.mz));
-      display.setCursor(72, 24); display.print("mG");
-
-      display.setCursor(0,  32); display.print((int)(myIMU.yaw));
-      display.setCursor(24, 32); display.print((int)(myIMU.pitch));
-      display.setCursor(48, 32); display.print((int)(myIMU.roll));
-      display.setCursor(66, 32); display.print("ypr");
-
-    // With these settings the filter is updating at a ~145 Hz rate using the
-    // Madgwick scheme and >200 Hz using the Mahony scheme even though the
-    // display refreshes at only 2 Hz. The filter update rate is determined
-    // mostly by the mathematical steps in the respective algorithms, the
-    // processor speed (8 MHz for the 3.3V Pro Mini), and the magnetometer ODR:
-    // an ODR of 10 Hz for the magnetometer produce the above rates, maximum
-    // magnetometer ODR of 100 Hz produces filter update rates of 36 - 145 and
-    // ~38 Hz for the Madgwick and Mahony schemes, respectively. This is
-    // presumably because the magnetometer read takes longer than the gyro or
-    // accelerometer reads. This filter update rate should be fast enough to
-    // maintain accurate platform orientation for stabilization control of a
-    // fast-moving robot or quadcopter. Compare to the update rate of 200 Hz
-    // produced by the on-board Digital Motion Processor of Invensense's MPU6050
-    // 6 DoF and MPU9150 9DoF sensors. The 3.3 V 8 MHz Pro Mini is doing pretty
-    // well!
-      display.setCursor(0, 40); display.print("rt: ");
-      display.print((float) myIMU.sumCount / myIMU.sum, 2);
-      display.print(" Hz");
-      display.display();
-#endif // LCD
 
       myIMU.count = millis();
       myIMU.sumCount = 0;
       myIMU.sum = 0;
-    } // if (myIMU.delt_t > 500)
-  } // if (AHRS)
 }
